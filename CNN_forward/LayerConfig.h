aac2d7d556c17086e58437c4f7d40413e61ba3ba
nViso <tim.llewellynn@nviso.ch>
2016-05-11 15:27:44 +0200

    suport add other layers to result layer, add english annotation

--- CNN_forward/LayerConfig.h
+++ CNN_forward/LayerConfig.h
@@ -22,17 +22,21 @@ public:
 	char type;//c:conv;p:pooling;d:dense;r:relu;i:input
 	int method;//For pooling
 	int poolsize;//For pooling
-	bool is_output;//是否算最后结果
-	int w, h;//resize的长宽
+	bool is_output;//mark if is a output layer
+	int w, h;//resize weight and height
+	int pad;// the number of padding, default 0
 	LayerConfig(std::string name, char type, bool is_output);
+	LayerConfig(std::string name, char type, int pad);
+	LayerConfig(std::string name, char type, int pad, bool is_output);
 	LayerConfig(std::string name, char type);
 	LayerConfig(std::string name, char type, std::vector<std::string> parent_names);
 	LayerConfig(std::string name, char type, std::vector<std::string> parent_names, bool is_output);
 	LayerConfig(std::string name, char type, std::string parent_name);
 	LayerConfig(std::string name, char type, std::string parent_name, bool is_output);
 	LayerConfig(std::string name, char type, int w, int h);
+	LayerConfig(std::string name, char type, int w, int h, bool is_output);
 private:
-	void init_layer(std::string name, char type, std::vector<std::string> parent_names, bool is_output);
+	void init_layer(std::string name, char type, std::vector<std::string> parent_names, bool is_output,int pad=0);
 };
 
 #endif
